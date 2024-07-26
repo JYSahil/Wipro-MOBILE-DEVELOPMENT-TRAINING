cd5f7b51fccbb0b73be7da27f17942a45bbc43e4
JYSahil <55951829+JYSahil@users.noreply.github.com>
2024-07-26 17:17:10 +0530

    Delete Schedulable.kt

--- Schedulable.kt
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime
-
-interface Schedulable {
-    val name: String
-    var scheduledTime: LocalDateTime
-
-    fun schedule(time: LocalDateTime)
-    fun reschedule(newTime: LocalDateTime)
-}
